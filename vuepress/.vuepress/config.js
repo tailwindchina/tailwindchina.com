96419b01beadf58d8b2c33b62ccc18deeeb9e231
codesinging <codesinging@gmail.com>
2020-12-16 18:07:45 +0800

    modify nav link

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
             { text: '使用教程', link: '/guides/' },
             { text: '中文文档', link: 'http://docs.tailwindchina.com' },
             { text: '相关资源', link: '/resources/' },
-            { text: '社区交流', link: 'https://github.com/tailwindchina/tailwindchina.com/discussions' },
+            { text: '交流社区', link: 'https://github.com/tailwindchina/tailwindchina.com/discussions' },
             { text: '英文网站', link: 'https://tailwindcss.com/' },
             { text: 'GitHub', link: 'https://github.com/tailwindchina/tailwindchina.com' }
         ]
