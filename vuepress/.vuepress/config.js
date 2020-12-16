03f6416aa80d5479197fac69c36e3b03ad450fcc
codesinging <codesinging@gmail.com>
2020-12-16 17:20:44 +0800

    add nav link: discussions

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -8,8 +8,9 @@ module.exports = {
             { text: '使用教程', link: '/guides/' },
             { text: '中文文档', link: 'http://docs.tailwindchina.com' },
             { text: '相关资源', link: '/resources/' },
+            { text: '社区交流', link: 'https://github.com/tailwindchina/tailwindchina.com/discussions' },
             { text: '英文网站', link: 'https://tailwindcss.com/' },
-            { text: 'GitHub', link: 'https://github.com/tailwindchina' }
+            { text: 'GitHub', link: 'https://github.com/tailwindchina/tailwindchina.com' }
         ]
     }
 }
