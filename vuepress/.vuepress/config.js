32479d2388a08c9392c9e95dcf1ad2e4025872e8
neoney <zairl23@126.com>
2021-01-07 18:52:12 +0800

    Fix spelling

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'TailWindCSS 中文网',
     description: 'TailwindCSS 使用教程、TailwindCSS 中文文档及 TailwindCSS 相关资源',
     head: [
-        ['meta', {name: 'keywords', content: 'TailwindCSS中文网, TailwindCCSS中文文档, TailwindCCSS文档, TailwindCSS中国, TailwindCSS教程'}],
+        ['meta', {name: 'keywords', content: 'TailwindCSS中文网, TailwindCSS中文文档, TailwindCSS文档, TailwindCSS中国, TailwindCSS教程'}],
         ['link', { rel: 'icon', href: '/logo.png' }]
     ],
     dest: './docs',
